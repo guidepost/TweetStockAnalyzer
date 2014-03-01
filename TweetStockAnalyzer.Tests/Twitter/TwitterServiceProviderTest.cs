56dd128eb7f60ae7f1f7f6a0fbd852cd2d4fc349
Misica <syou777jp@gmail.com>
2014-03-01 22:45:14 +0900

    テストの追加

---
+++ TweetStockAnalyzer.Tests/Twitter/TwitterServiceProviderTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TweetStockAnalyzer.Twitter;
+using TweetSharp;
+
+namespace TweetStockAnalyzer.Tests.Twitter
+{
+    [TestClass]
+    public class TwitterServiceProviderTest
+    {
+        [TestMethod]
+        public void GetAuthenticatedService()
+        {
+            var provider = new TwitterServiceProvider();
+            var service = provider.GetAuthenticatedService();
+
+            var result = service.Search(new SearchOptions()
+            {
+                Q = "ハピプリ"
+            });
+
+            (result.Statuses.Count() > 0).IsTrue();
+        }
+    }
+}
