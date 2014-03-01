f128bb9c69c1a87ff8e156ea5f0c5a8609e42e1f
Misica <syou777jp@gmail.com>
2014-03-02 00:40:02 +0900

    RepositoryBaseに実装されたIDisposableを利用するため

---
+++ TweetStockAnalyzerWeb/Controllers/CompanyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TweetStockAnalyzerWeb.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+	}
+}
---
+++ TweetStockAnalyzerWeb/WorkerService/CompanyWorkerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TweetStockAnalyzer.DataBase;
+using TweetStockAnalyzer.Infrastructure.Dependency;
+using TweetStockAnalyzerWeb.ViewModel;
+
+namespace TweetStockAnalyzerWeb.WorkerService
+{
+    public class CompanyWorkerService
+    {
+        public CompanyIndexViewModel GetIndexViewModel()
+        {
+            //using (var repository = new CompanyRepository())
+            //{
+            //    return new CompanyIndexViewModel
+            //    {
+            //        Companies = repository.ReadAll()
+            //    };
+            //}
+            return null;
+        }
+    }
+}
