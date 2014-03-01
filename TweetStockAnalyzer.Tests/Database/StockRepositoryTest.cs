80c52ff6132e76833954f91399b51930d124425a
yt <test@test.com>
2014-03-01 14:12:13 +0900

    モデル追加

---
+++ TweetStockAnalyzer.Tests/Database/StockRepositoryTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TweetStockAnalyzer.DataBase;
+
+namespace TweetStockAnalyzer.Tests.Database
+{
+    [TestClass]
+    public class StockRepositoryTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            var notFoundKey = -100;
+            var repository = new StockRepository();
+            var result = repository.Read(notFoundKey);
+            Assert.IsNull(result);
+        }
+    }
+}
---
+++ TweetStockAnalyzer/DataBase/RepositoryBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetStockAnalyzer.Model;
+
+namespace TweetStockAnalyzer.DataBase
+{
+    public abstract class RepositoryBase<T> : IRepository<T>
+    {
+        protected TweetStockAnalyzerEntities Entities { get; private set; }
+
+        protected RepositoryBase()
+        {
+            Entities = new TweetStockAnalyzerEntities();
+        }
+
+        public abstract T Create();
+        public abstract T Read(int id);
+        public abstract void Update(T valud);
+        public abstract void Delte(int id);
+    }
+}
