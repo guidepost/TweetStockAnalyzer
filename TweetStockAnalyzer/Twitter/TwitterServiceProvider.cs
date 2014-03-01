a108c200dbe0edc3dc273d964c048094bf8698a1
Misica <syou777jp@gmail.com>
2014-03-01 17:37:22 +0900

    TwitterServiceProvider作成

---
+++ TweetStockAnalyzer/Twitter/TwitterServiceProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetSharp;
+
+namespace TweetStockAnalyzer.Twitter
+{
+    public class TwitterServiceProvider
+    {
+        public TwitterService GetAuthenticatedService()
+        {
+            string comsumerKey = ConfigurationManager.AppSettings["TwitterComsumerKey"];
+            string comsumerSecret = ConfigurationManager.AppSettings["TwitterComsumerSecret"];
+            string accessToken = ConfigurationManager.AppSettings["TwitterAccessToken"];
+            string accessTokenSecret = ConfigurationManager.AppSettings["TwitterAccessTokenSecret"];
+
+            var service = new TwitterService(comsumerKey, comsumerSecret);
+            service.AuthenticateWith(accessToken, accessTokenSecret);
+
+            return service;
+        }
+    }
+}
