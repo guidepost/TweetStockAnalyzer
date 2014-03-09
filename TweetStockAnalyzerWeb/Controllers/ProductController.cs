9aefce2d0b356e161f2080f28dc40e296596bbe8
Misica <syou777jp@gmail.com>
2014-03-09 19:36:56 +0900

    とりあえずProduct関係のクラスとかフォルダとか追加

--- TweetStockAnalyzerWeb/Controllers/ProductController.cs
+++ TweetStockAnalyzerWeb/Controllers/ProductController.cs
@@ -1,20 +1,49 @@
-﻿using System;
+﻿using Microsoft.Practices.Unity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TweetStockAnalyzer.Infrastructure.Dependency;
+using TweetStockAnalyzerWeb.WorkerService;
 
 namespace TweetStockAnalyzerWeb.Controllers
 {
     public class ProductController : Controller
     {
-        //
-        // GET: /Product/
+        private IUnityContainer _container = DependencyContainer.Instance;
+
+        private ProductWorkerService _workerService = new ProductWorkerService();
+
         public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult Create()
+        {
+            return View();
+        }
 
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Update(int productId)
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return RedirectToIndex();
+        }
+
+        private ActionResult RedirectToIndex(string successMessage = null)
+        {
+            return RedirectToAction("Index", new { successMessage = successMessage });
+        }
 	}
 }
---
+++ TweetStockAnalyzerWeb/WorkerService/ProductWorkerService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TweetStockAnalyzer.Infrastructure.Dependency;
+using TweetStockAnalyzer.Model;
+using TweetStockAnalyzerWeb.ViewModel.Product;
+
+namespace TweetStockAnalyzerWeb.WorkerService
+{
+    public class ProductWorkerService
+    {
+        private IUnityContainer _container = DependencyContainer.Instance;
+
+        public ProductIndexViewModel GetIndexViewModel()
+        {
+            var viewModel = new ProductIndexViewModel();
+
+            return viewModel;
+        }
+
+        public void CreateProduct()
+        {
+        }
+
+        public void UpdateProduct(Product product)
+        {
+        }
+
+        public void DeleteProduct()
+        {
+        }
+    }
+}
