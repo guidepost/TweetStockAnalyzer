579b4f193a9b00a42c065213bb2a8f91a9e88beb
Misica <syou777jp@gmail.com>
2014-03-13 19:54:13 +0900

    リベース

---
+++ TweetStockAnalyzerWeb/ViewModel/Product/ProductDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TweetStockAnalyzerWeb.ViewModel.Product
+{
+    public class ProductDetailViewModel
+    {
+        public TweetStockAnalyzer.Model.Product Product { get; set; }
+    }
+}
