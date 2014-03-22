31b7d7122cba455b9416c503d294a1d3fe7fab6d
yt <test@test.com>
2014-03-22 14:37:09 +0900

    スコア計算を呼ぶ

---
+++ TweetStockAnalyzeSchedule/ScoreUpdator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using TweetStockAnalyzer.DataBase.Repository;
+using TweetStockAnalyzer.Domain.Score;
+using TweetStockAnalyzer.Infrastructure.Dependency;
+
+namespace TweetStockAnalyzeSchedule
+{
+    public class ScoreUpdator : ISchedule
+    {
+        public void Start()
+        {
+            var scoreCalculator = DependencyContainer.Instance.Resolve<IScoreCalculator>();
+            using (var companyScoreRepository = DependencyContainer.Instance.Resolve<ICompanyScoreRepository>())
+            using (var companyRepository = DependencyContainer.Instance.Resolve<ICompanyRepository>())
+            {
+                foreach (var company in companyRepository.ReadAll().ToList())
+                {
+                    var score = scoreCalculator.GetScore(company);
+                    companyScoreRepository.Create(company, score);
+                }
+            }
+        }
+    }
+}
